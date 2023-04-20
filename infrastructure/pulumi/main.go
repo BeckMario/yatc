70453096b6bd4bf418aac6420c712763390b7adb
Mario Beck <mail@mariobeck.eu>
2023-04-20 14:30:11 +0200

    add prometheus and grafana to pulumi

--- infrastructure/pulumi/main.go
+++ infrastructure/pulumi/main.go
@@ -96,125 +96,24 @@ func main() {
 			return err
 		}
 
-		daprNamespace, err := corev1.NewNamespace(ctx, "dapr-system", &corev1.NamespaceArgs{
-			Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("dapr-system")},
-		})
+		dapr, err := createDapr(ctx)
 		if err != nil {
 			return err
 		}
 
-		dapr, err := helm.NewChart(ctx, "dapr", helm.ChartArgs{
-			Version:   pulumi.String("1.10"),
-			Chart:     pulumi.String("dapr"),
-			Namespace: daprNamespace.Metadata.Elem().Name().Elem(),
-			FetchArgs: &helm.FetchArgs{
-				Repo: pulumi.String("https://dapr.github.io/helm-charts/"),
-			},
-		}, pulumi.DependsOn([]pulumi.Resource{daprNamespace}), pulumi.Transformations([]pulumi.ResourceTransformation{
-			// Source: https://www.pulumi.com/registry/packages/kubernetes/how-to-guides/managing-resources-with-server-side-apply/#helm-charts
-			// Ignore changes that will be overwritten by the kruise-manager deployment.
-			func(args *pulumi.ResourceTransformationArgs) *pulumi.ResourceTransformationResult {
-				if args.Type == "kubernetes:admissionregistration.k8s.io/v1:ValidatingWebhookConfiguration" ||
-					args.Type == "kubernetes:admissionregistration.k8s.io/v1:MutatingWebhookConfiguration" {
-					return &pulumi.ResourceTransformationResult{
-						Props: args.Props,
-						Opts: append(args.Opts, pulumi.IgnoreChanges([]string{
-							"metadata.annotations.template",
-							"webhooks[*].clientConfig",
-						})),
-					}
-				}
-
-				if args.Name == "dapr-system/dapr-webhook-ca" ||
-					args.Name == "dapr-system/dapr-webhook-cert" ||
-					args.Name == "dapr-system/dapr-sidecar-injector-cert" {
-					return &pulumi.ResourceTransformationResult{
-						Props: args.Props,
-						Opts: append(args.Opts, pulumi.IgnoreChanges([]string{
-							"data",
-						})),
-					}
-				}
-				return nil
-			},
-		}))
+		minio, err := createMinio(ctx)
 		if err != nil {
 			return err
 		}
 
-		minioNamespace, err := corev1.NewNamespace(ctx, "minio", &corev1.NamespaceArgs{
-			Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("minio")},
-		})
+		redis, err := createRedis(ctx)
 		if err != nil {
 			return err
 		}
 
-		minio, err := helm.NewChart(ctx, "minio", helm.ChartArgs{
-			Version:   pulumi.String("12.2.4"),
-			Chart:     pulumi.String("minio"),
-			Namespace: minioNamespace.Metadata.Elem().Name().Elem(),
-			FetchArgs: &helm.FetchArgs{
-				Repo: pulumi.String("https://charts.bitnami.com/bitnami"),
-			},
-			Values: pulumi.Map{
-				"auth": pulumi.Map{
-					"rootUser":     pulumi.String("minioadmin"),
-					"rootPassword": pulumi.String("minioadmin"),
-				},
-				"defaultBuckets": pulumi.String("testbucket"),
-			},
-		}, pulumi.DependsOn([]pulumi.Resource{minioNamespace}))
-		if err != nil {
-			return err
-		}
-
-		redis, err := helm.NewChart(ctx, "redis", helm.ChartArgs{
-			Version: pulumi.String("17.9.4"),
-			Chart:   pulumi.String("redis"),
-			FetchArgs: &helm.FetchArgs{
-				Repo: pulumi.String("https://charts.bitnami.com/bitnami"),
-			},
-			Values: pulumi.Map{
-				"auth": pulumi.Map{
-					"password": pulumi.String("redis"),
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		openfunctionNamespace, err := corev1.NewNamespace(ctx, "openfunction", &corev1.NamespaceArgs{
-			Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("openfunction")},
-		})
-		if err != nil {
-			return err
-		}
-
-		openfunction, err := helm.NewRelease(ctx, "openfunction", &helm.ReleaseArgs{
-			Name:      pulumi.String("openfunction"),
-			Version:   pulumi.String("0.5.0"),
-			Chart:     pulumi.String("openfunction"),
-			Namespace: openfunctionNamespace.Metadata.Elem().Name().Elem(),
-			RepositoryOpts: &helm.RepositoryOptsArgs{
-				Repo: pulumi.String("https://openfunction.github.io/charts/"),
-			},
-			Values: pulumi.Map{
-				"global": pulumi.Map{
-					"Contour": pulumi.Map{
-						"enabled": pulumi.Bool(false),
-					},
-					"KnativeServing": pulumi.Map{
-						"enabled": pulumi.Bool(false),
-					},
-					"Dapr": pulumi.Map{
-						"enabled": pulumi.Bool(false),
-					},
-				},
-			},
-		}, pulumi.DependsOn([]pulumi.Resource{openfunctionNamespace}), pulumi.DependsOnInputs(dapr.Ready))
-		if err != nil {
-			return err
+		openfunction, err2 := createOpenFunction(ctx, dapr)
+		if err2 != nil {
+			return err2
 		}
 
 		daprComponents, err := yaml.NewConfigGroup(ctx, "dapr-components", &yaml.ConfigGroupArgs{
@@ -290,6 +189,11 @@ func main() {
 			return err
 		}
 
+		err = createDaprMonitoring(ctx)
+		if err != nil {
+			return err
+		}
+
 		for _, service := range services {
 			_, err := NewServiceDeployment(ctx, service, dependencies...)
 			if err != nil {
@@ -299,4 +203,196 @@ func main() {
 
 		return nil
 	})
+}
+
+func createOpenFunction(ctx *pulumi.Context, dapr *helm.Chart) (*helm.Release, error) {
+	openfunctionNamespace, err := corev1.NewNamespace(ctx, "openfunction", &corev1.NamespaceArgs{
+		Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("openfunction")},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	openfunction, err := helm.NewRelease(ctx, "openfunction", &helm.ReleaseArgs{
+		Name:      pulumi.String("openfunction"),
+		Version:   pulumi.String("0.5.0"),
+		Chart:     pulumi.String("openfunction"),
+		Namespace: openfunctionNamespace.Metadata.Elem().Name().Elem(),
+		RepositoryOpts: &helm.RepositoryOptsArgs{
+			Repo: pulumi.String("https://openfunction.github.io/charts/"),
+		},
+		Values: pulumi.Map{
+			"global": pulumi.Map{
+				"Contour": pulumi.Map{
+					"enabled": pulumi.Bool(false),
+				},
+				"KnativeServing": pulumi.Map{
+					"enabled": pulumi.Bool(false),
+				},
+				"Dapr": pulumi.Map{
+					"enabled": pulumi.Bool(false),
+				},
+			},
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{openfunctionNamespace}), pulumi.DependsOnInputs(dapr.Ready))
+	if err != nil {
+		return nil, err
+	}
+	return openfunction, nil
+}
+
+func createRedis(ctx *pulumi.Context) (*helm.Chart, error) {
+	redis, err := helm.NewChart(ctx, "redis", helm.ChartArgs{
+		Version: pulumi.String("17.9.4"),
+		Chart:   pulumi.String("redis"),
+		FetchArgs: &helm.FetchArgs{
+			Repo: pulumi.String("https://charts.bitnami.com/bitnami"),
+		},
+		Values: pulumi.Map{
+			"auth": pulumi.Map{
+				"password": pulumi.String("redis"),
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return redis, nil
+}
+
+func createMinio(ctx *pulumi.Context) (*helm.Chart, error) {
+	minioNamespace, err := corev1.NewNamespace(ctx, "minio", &corev1.NamespaceArgs{
+		Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("minio")},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	minio, err := helm.NewChart(ctx, "minio", helm.ChartArgs{
+		Version:   pulumi.String("12.2.4"),
+		Chart:     pulumi.String("minio"),
+		Namespace: minioNamespace.Metadata.Elem().Name().Elem(),
+		FetchArgs: &helm.FetchArgs{
+			Repo: pulumi.String("https://charts.bitnami.com/bitnami"),
+		},
+		Values: pulumi.Map{
+			"auth": pulumi.Map{
+				"rootUser":     pulumi.String("minioadmin"),
+				"rootPassword": pulumi.String("minioadmin"),
+			},
+			"defaultBuckets": pulumi.String("testbucket"),
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{minioNamespace}))
+	if err != nil {
+		return nil, err
+	}
+	return minio, nil
+}
+
+func createDapr(ctx *pulumi.Context) (*helm.Chart, error) {
+	daprNamespace, err := corev1.NewNamespace(ctx, "dapr-system", &corev1.NamespaceArgs{
+		Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("dapr-system")},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	dapr, err := helm.NewChart(ctx, "dapr", helm.ChartArgs{
+		Version:   pulumi.String("1.10"),
+		Chart:     pulumi.String("dapr"),
+		Namespace: daprNamespace.Metadata.Elem().Name().Elem(),
+		FetchArgs: &helm.FetchArgs{
+			Repo: pulumi.String("https://dapr.github.io/helm-charts/"),
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{daprNamespace}), pulumi.Transformations([]pulumi.ResourceTransformation{
+		// Source: https://www.pulumi.com/registry/packages/kubernetes/how-to-guides/managing-resources-with-server-side-apply/#helm-charts
+		// Ignore changes that will be overwritten by the kruise-manager deployment.
+		func(args *pulumi.ResourceTransformationArgs) *pulumi.ResourceTransformationResult {
+			if args.Type == "kubernetes:admissionregistration.k8s.io/v1:ValidatingWebhookConfiguration" ||
+				args.Type == "kubernetes:admissionregistration.k8s.io/v1:MutatingWebhookConfiguration" {
+				return &pulumi.ResourceTransformationResult{
+					Props: args.Props,
+					Opts: append(args.Opts, pulumi.IgnoreChanges([]string{
+						"metadata.annotations.template",
+						"webhooks[*].clientConfig",
+					})),
+				}
+			}
+
+			if args.Name == "dapr-system/dapr-webhook-ca" ||
+				args.Name == "dapr-system/dapr-webhook-cert" ||
+				args.Name == "dapr-system/dapr-sidecar-injector-cert" {
+				return &pulumi.ResourceTransformationResult{
+					Props: args.Props,
+					Opts: append(args.Opts, pulumi.IgnoreChanges([]string{
+						"data",
+					})),
+				}
+			}
+			return nil
+		},
+	}))
+	if err != nil {
+		return nil, err
+	}
+	return dapr, nil
+}
+
+func createDaprMonitoring(ctx *pulumi.Context) error {
+	daprMonitoring, err := corev1.NewNamespace(ctx, "dapr-monitoring", &corev1.NamespaceArgs{
+		Metadata: &metav1.ObjectMetaArgs{Name: pulumi.String("dapr-monitoring")},
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = helm.NewChart(ctx, "prometheus", helm.ChartArgs{
+		Version:   pulumi.String("20.2.1"),
+		Chart:     pulumi.String("prometheus"),
+		Namespace: daprMonitoring.Metadata.Elem().Name().Elem(),
+		FetchArgs: &helm.FetchArgs{
+			Repo: pulumi.String("https://prometheus-community.github.io/helm-charts"),
+		},
+		Values: pulumi.Map{
+			"server": pulumi.Map{
+				"persistentVolume": pulumi.Map{
+					"enabled": pulumi.Bool(false),
+				},
+			},
+			"alertmanager": pulumi.Map{
+				"enabled": pulumi.Bool(false),
+			},
+			"prometheus-pushgateway": pulumi.Map{
+				"enabled": pulumi.Bool(false),
+			},
+			"prometheus-node-exporter": pulumi.Map{
+				"enabled": pulumi.Bool(false),
+			},
+			"kube-state-metrics": pulumi.Map{
+				"enabled": pulumi.Bool(false),
+			},
+			"configmapReload": pulumi.Map{
+				"enabled": pulumi.Bool(false),
+			},
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{daprMonitoring}))
+	if err != nil {
+		return err
+	}
+
+	_, err = helm.NewChart(ctx, "grafana", helm.ChartArgs{
+		Version:   pulumi.String("6.54.0"),
+		Chart:     pulumi.String("grafana"),
+		Namespace: daprMonitoring.Metadata.Elem().Name().Elem(),
+		FetchArgs: &helm.FetchArgs{
+			Repo: pulumi.String("https://grafana.github.io/helm-charts"),
+		},
+		Values: pulumi.Map{
+			"adminPassword": pulumi.String("admin"),
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{daprMonitoring}))
+	if err != nil {
+		return err
+	}
+	return nil
 }
