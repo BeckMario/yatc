3a3410df3757cc6fbbe76a7a3d622b7ad5602774
Mario Beck <mail@mariobeck.eu>
2023-05-03 17:41:16 +0200

    add docker compose to easily start dependencies for microservices

--- magefiles/mage.go
+++ magefiles/mage.go
@@ -5,8 +5,22 @@ package main
 import (
 	"context"
 	"dagger.io/dagger"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 	"os"
 )
+
+type Docker mg.Namespace
+
+// Up Start needed dependencies with docker compose
+func (d Docker) Up() error {
+	return sh.Run("docker-compose", "-f", "infrastructure/local/docker-compose.yaml", "up", "-d")
+}
+
+// Down Stop dependencies with docker compose
+func (d Docker) Down() error {
+	return sh.Run("docker-compose", "-f", "infrastructure/local/docker-compose.yaml", "down")
+}
 
 func Lint() error {
 	client, err := dagger.Connect(context.Background(), dagger.WithLogOutput(os.Stdout))
